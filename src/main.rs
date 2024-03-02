5aba5fd1d76d76b0ed73bbfe5a645ab7eb36ff4e
Richard Giraud <richard@rgent.ca>
2024-03-02 13:16:14 -0700

    Render empty map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn render_map() {
+    print!("  ");
+    for x in 0..0x10 {
+        print!("{:0x} ", x);
+    }
+    print!("\n");
+
+    for y in 0..0x10 {
+        print!("{:0x} ", y);
+        for _x in 0..0x10 {
+            print!(". ");
+        }
+        print!("\n");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    render_map();
 }
